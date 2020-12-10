6355baacdff4a477056ec69971b268b5d9a0b373
xuech <xuech_love@126.com>
2020-12-10 14:58:20 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,54 @@
 <template>
   <div id="app">
+    <cascader :options=options></cascader>
   </div>
 </template>
 
 <script>
 
+import Cascader from './components/Cascader.vue';
+
 export default {
   name: 'App',
+  components: {
+    Cascader,
+  },
   data() {
     return {
-      title: 'x',
+      options: [
+        {
+          label: '肉类',
+          children: [
+            {
+              label: '猪肉',
+              children: [
+                {
+                  label: '五花肉',
+                },
+                {
+                  label: '里脊肉',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: '乳类',
+          children: [
+            {
+              label: '牛奶',
+              children: [
+                {
+                  label: '伊犁',
+                },
+                {
+                  label: '蒙牛',
+                },
+              ],
+            },
+          ],
+        },
+      ],
 
     };
   },
---
+++ src/components/Cascader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cascader">
+    <div class="title">123</div>
+    <div class="content"></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cascader',
+  props: {
+    options: {
+      type: Object,
+      default: () => { },
+    },
+  },
+  data () {
+    return {
+      isVisible: false,
+    },
+  }
+};
+
+</script>
+
+<style lang="stylus">
+.title {
+  width: 150px;
+  height: 30px;
+}
+</style>
