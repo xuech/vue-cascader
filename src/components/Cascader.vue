eec18448c52bef44a9550bf9778b97d3afcdf22d
xuech <xuech_love@126.com>
2020-12-10 14:58:37 +0800

    ss

--- src/components/Cascader.vue
+++ src/components/Cascader.vue
@@ -1,27 +1,47 @@
 <template>
-  <div class="cascader">
-    <div class="title">123</div>
-    <div class="content"></div>
-
+  <div class="cascader" v-click-outside="close">
+    <div class="title" @click="toggle">123</div>
+    <div class="content" v-if="isVisible">显示的内容</div>
   </div>
 </template>
 
 <script>
 export default {
+  /// 只在组件内部使用
+  directives: {
+    clickOutside: {
+      /// 指令的生命周期
+      inserted(el, bindings) {
+        document.addEventListener('click', (e) => {
+          if (e.target === el || el.contains(e.target)) {
+            return;
+          }
+          bindings.value();
+        });
+      },
+    },
+  },
   name: 'Cascader',
   props: {
     options: {
       type: Object,
-      default: () => { },
+      default: () => {},
     },
   },
-  data () {
+  data() {
     return {
       isVisible: false,
+    };
+  },
+  methods: {
+    close() {
+      this.isVisible = false;
     },
-  }
+    toggle() {
+      this.isVisible = !this.isVisible;
+    },
+  },
 };
-
 </script>
 
 <style lang="stylus">
