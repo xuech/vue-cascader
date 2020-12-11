0c2bf79cfa1625f5039f5e900891e7028abc9f79
xuech <xuech_love@126.com>
2020-12-11 17:28:37 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <cascader :options=options :value="value" v-model="value"></cascader>
+    <cascader :options=options  v-model="value"></cascader>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 import Cascader from './components/Cascader.vue';
 
 export default {
@@ -16,41 +16,14 @@ export default {
   data() {
     return {
       value: [],
-      options: [
-        {
-          label: '肉类',
-          children: [
-            {
-              label: '猪肉',
-              children: [
-                {
-                  label: '五花肉',
-                },
-                {
-                  label: '里脊肉',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          label: '乳类',
-          children: [
-            {
-              label: '牛奶',
-              children: [
-                {
-                  label: '伊犁',
-                },
-                {
-                  label: '蒙牛',
-                },
-              ],
-            },
-          ],
-        },
-      ],
+      options: [],
     };
+  },
+  created() {
+    axios.get('http://localhost:3000/api/foods').then((res) => {
+      console.log(res);
+      this.options = res.data;
+    });
   },
 };
 </script>
